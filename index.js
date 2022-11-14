4ed29cb00e7e9f087a428a73f4e54310c2d6561d
David Ben Moshe <110648029+DBenMoshe@users.noreply.github.com>
2022-11-14 05:34:09 +0200

    Adding  files

---
+++ index.js
@@ -0,0 +1,36 @@
+let homePoints = document.getElementById("home-points")
+let guestPoints = document.getElementById("guest-points")
+
+let hPoints = 0
+let gPoints = 0
+
+function homeOne() {
+    hPoints++
+    homePoints.textContent = hPoints
+}
+
+function homeTwo() {
+    hPoints += 2
+    homePoints.textContent = hPoints
+}
+
+function homeThree() {
+    hPoints += 3
+    homePoints.textContent = hPoints
+}
+
+function guestOne() {
+    gPoints++
+    guestPoints.textContent = gPoints
+}
+
+function guestTwo() {
+    gPoints += 2
+    guestPoints.textContent = gPoints
+}
+
+function guestThree() {
+    gPoints += 3
+    guestPoints.textContent = gPoints
+}
+
